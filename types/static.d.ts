775279d662e500fda1bcb355611b356664b0c9c0
helloyeew <gameforphawit@outlook.com>
2020-12-08 02:03:54 +0700

    ✅ Add more bugs and fix it later

---
+++ types/static.d.ts
@@ -0,0 +1,11 @@
+declare module '*.css';
+
+declare module '*.jpg' {
+    const ref: string;
+    export default ref;
+  }
+
+  declare module '*.png' {
+    const ref: string;
+    export default ref;
+  }
