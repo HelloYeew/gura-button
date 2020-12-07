ad845853abc4d211a5638cc75de35b280b87ecce
helloyeew <gameforphawit@outlook.com>
2020-12-08 01:40:34 +0700

    🎉 Initial release

---
+++ ts/gura-button.ts
@@ -0,0 +1,7 @@
+import { LitElement, html, css } from 'lit-element'
+import { customElement, internalProperty, property } from 'lit-element/lib/decorators'
+
+@customElement('gura-button')
+export class GuraButton extends LitElement {
+    
+}
---
+++ ts/index.ts
@@ -0,0 +1,1 @@
+import './gura-button'
